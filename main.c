a6a374c1fda31c567fdd084338fac653a71bad21
Mike <mike@flomp.net>
2014-05-19 18:07:46 +0100

    Remove mouse reports, fixes media buttons on Android
    Workaround for Android bug:
        https://code.google.com/p/android/issues/detail?id=63642

--- main.c
+++ main.c
@@ -115,16 +115,10 @@
 #define SEC_PARAM_MAX_KEY_SIZE          16                                          /**< Maximum encryption key size. */
 
 #define MOVEMENT_SPEED                  25                                          /**< Number of pixels by which the cursor is moved each time a button is pushed. */
-#define INPUT_REPORT_COUNT              3                                           /**< Number of input reports in this application. */
-#define INPUT_REP_BUTTONS_LEN           3                                           /**< Length of Mouse Input Report containing button data. */
-#define INPUT_REP_MOVEMENT_LEN          3                                           /**< Length of Mouse Input Report containing movement data. */
+#define INPUT_REPORT_COUNT              1                                           /**< Number of input reports in this application. */
 #define INPUT_REP_MEDIA_PLAYER_LEN      1                                           /**< Length of Mouse Input Report containing media player data. */
-#define INPUT_REP_BUTTONS_INDEX         0                                           /**< Index of Mouse Input Report containing button data. */
-#define INPUT_REP_MOVEMENT_INDEX        1                                           /**< Index of Mouse Input Report containing movement data. */
-#define INPUT_REP_MPLAYER_INDEX         2                                           /**< Index of Mouse Input Report containing media player data. */
-#define INPUT_REP_REF_BUTTONS_ID        1                                           /**< Id of reference to Mouse Input Report containing button data. */
-#define INPUT_REP_REF_MOVEMENT_ID       2                                           /**< Id of reference to Mouse Input Report containing movement data. */
-#define INPUT_REP_REF_MPLAYER_ID        3                                           /**< Id of reference to Mouse Input Report containing media player data. */
+#define INPUT_REP_MPLAYER_INDEX         0                                           /**< Index of Mouse Input Report containing media player data. */
+#define INPUT_REP_REF_MPLAYER_ID        1                                           /**< Id of reference to Mouse Input Report containing media player data. */
 
 #define MPLAYER_PLAYPAUSE               0
 #define MPLAYER_NEXT                    2
@@ -421,59 +415,11 @@ static void hids_init(void)
 
     static uint8_t rep_map_data[] =
     {
-        0x05, 0x01,                     // Usage Page (Generic Desktop)
-        0x09, 0x02,                     // Usage (Mouse)
-
-        0xA1, 0x01,                     // Collection (Application)
-
-        // Report ID 1: Mouse buttons + scroll/pan
-        0x85, 0x01,                     //     Report Id 1
-        0x09, 0x01,                     //     Usage (Pointer)
-        0xA1, 0x00,                     //     Collection (Physical)
-        0x95, 0x05,                     //         Report Count (3)
-        0x75, 0x01,                     //         Report Size (1)
-        0x05, 0x09,                     //         Usage Page (Buttons)
-        0x19, 0x01,                     //             Usage Minimum (01)
-        0x29, 0x05,                     //             Usage Maximum (05)
-        0x15, 0x00,                     //             Logical Minimum (0)
-        0x25, 0x01,                     //             Logical Maximum (1)
-        0x81, 0x02,                     //             Input (Data, Variable, Absolute)
-        0x95, 0x01,                     //             Report Count (1)
-        0x75, 0x03,                     //             Report Size (3)
-        0x81, 0x01,                     //             Input (Constant) for padding
-        0x75, 0x08,                     //             Report Size (8)
-        0x95, 0x01,                     //             Report Count (1)
-        0x05, 0x01,                     //         Usage Page (Generic Desktop)
-        0x09, 0x38,                     //             Usage (Wheel)
-        0x15, 0x81,                     //             Logical Minimum (-127)
-        0x25, 0x7F,                     //             Logical Maximum (127)
-        0x81, 0x06,                     //             Input (Data, Variable, Relative)
-        0x05, 0x0C,                     //         Usage Page (Consumer)
-        0x0A, 0x38, 0x02,               //             Usage (AC Pan)
-        0x95, 0x01,                     //             Report Count (1)
-        0x81, 0x06,                     //             Input (Data,Value,Relative,Bit Field)
-        0xC0,                           //     End Collection (Physical)
-
-        // Report ID 2: Mouse motion
-        0x85, 0x02,                     //     Report Id 2
-        0x09, 0x01,                     //     Usage (Pointer)
-        0xA1, 0x00,                     //     Collection (Physical)
-        0x75, 0x0C,                     //         Report Size (12)
-        0x95, 0x02,                     //         Report Count (2)
-        0x05, 0x01,                     //         Usage Page (Generic Desktop)
-        0x09, 0x30,                     //             Usage (X)
-        0x09, 0x31,                     //             Usage (Y)
-        0x16, 0x01, 0xF8,               //             Logical maximum (2047)
-        0x26, 0xFF, 0x07,               //             Logical minimum (-2047)
-        0x81, 0x06,                     //             Input (Data, Variable, Relative)
-        0xC0,                           //     End Collection (Physical)
-        0xC0,                           // End Collection (Application)
-
-        // Report ID 3: Advanced buttons
+        // Report ID 1: Advanced buttons
         0x05, 0x0C,                     // Usage Page (Consumer)
         0x09, 0x01,                     // Usage (Consumer Control)
         0xA1, 0x01,                     // Collection (Application)
-        0x85, 0x03,                     //     Report Id (3)
+        0x85, 0x01,                     //     Report Id (1)
         0x15, 0x00,                     //     Logical minimum (0)
         0x25, 0x01,                     //     Logical maximum (1)
         0x75, 0x01,                     //     Report Size (1)
@@ -500,24 +446,6 @@ static void hids_init(void)
     };
 
     // Initialize HID Service.
-    p_input_report                      = &inp_rep_array[INPUT_REP_BUTTONS_INDEX];
-    p_input_report->max_len             = INPUT_REP_BUTTONS_LEN;
-    p_input_report->rep_ref.report_id   = INPUT_REP_REF_BUTTONS_ID;
-    p_input_report->rep_ref.report_type = BLE_HIDS_REP_TYPE_INPUT;
-
-    BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&p_input_report->security_mode.cccd_write_perm);
-    BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&p_input_report->security_mode.read_perm);
-    BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&p_input_report->security_mode.write_perm);
-
-    p_input_report                      = &inp_rep_array[INPUT_REP_MOVEMENT_INDEX];
-    p_input_report->max_len             = INPUT_REP_MOVEMENT_LEN;
-    p_input_report->rep_ref.report_id   = INPUT_REP_REF_MOVEMENT_ID;
-    p_input_report->rep_ref.report_type = BLE_HIDS_REP_TYPE_INPUT;
-
-    BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&p_input_report->security_mode.cccd_write_perm);
-    BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&p_input_report->security_mode.read_perm);
-    BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&p_input_report->security_mode.write_perm);
-
     p_input_report                      = &inp_rep_array[INPUT_REP_MPLAYER_INDEX];
     p_input_report->max_len             = INPUT_REP_MEDIA_PLAYER_LEN;
     p_input_report->rep_ref.report_id   = INPUT_REP_REF_MPLAYER_ID;
